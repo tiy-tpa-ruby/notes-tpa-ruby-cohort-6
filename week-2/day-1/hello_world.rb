df8c8728869ccf4461d946db2cb1e30b16f6c075
Gavin Stark <gavin@gstark.com>
2016-11-14 13:13:53 -0500

    Corrected code by putting \<\< statement AFTER the comparison

--- week-2/day-1/hello_world.rb
+++ week-2/day-1/hello_world.rb
@@ -6,13 +6,13 @@ scores = []
 loop do
   puts "What is your score?"
   score = gets.chomp
-  scores << score
 
   if score == ""
     puts "Thanks, all done"
     break
   end
 
+  scores << score
 end
 
 puts "The person #{name.inspect} has #{scores.inspect} scores"
